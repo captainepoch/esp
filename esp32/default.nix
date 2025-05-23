74ef7abbfb495d85538671dbedda2e00aa57180b
Adolfo Santiago <epoch@adol.pw>
2025-05-23 16:10:19 +0200

    New env
    Signed-off-by: Adolfo Santiago <epoch@adol.pw>

--- esp32/default.nix
+++ esp32/default.nix
@@ -1,14 +1,7 @@
-# A standalone shell definition that overrides the versions of ESP-IDF
-# and the ESP32 toolchain.
-#
-# Repo: https://github.com/mirrexagon/nixpkgs-esp-dev
-
 let
   nixpkgs-esp-dev = builtins.fetchGit {
-    url = "https://github.com/mirrexagon/nixpkgs-esp-dev.git#esp32-idf";
+    url = "https://github.com/mirrexagon/nixpkgs-esp-dev.git";
   };
-
-  rev = "31ee58005f43e93a6264e3667c9bf5c31b368733";
 
   pkgs = import <nixpkgs> {
     overlays = [ (import "${nixpkgs-esp-dev}/overlay.nix") ];
